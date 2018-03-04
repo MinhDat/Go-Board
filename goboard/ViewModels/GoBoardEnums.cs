ea7091cfd820f7c07a15c13cc7624add6b3953c6
haminhdat <dat.ha@vexere.com>
2018-03-04 14:22:13 +0700

    first commit

---
+++ goboard/ViewModels/GoBoardEnums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoBoard.ViewModels
+{
+    public enum GoBoardAnnotationType
+    {
+        Circle,
+        Rectangle,
+        Triangle,
+        Label
+    }
+
+    public enum GoBoardHoverType
+    {
+        None,
+        Stone
+    }
+}
---
+++ goboard/ViewModels/Stone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoBoard.ViewModels
+{
+    public enum Stone : byte
+    {
+        Black = 0,
+        Red = 1,
+        Empty = 2,
+        Boundary = 3
+    }
+}
